994a8ae8657025f05e88f27cd46a4e14bac25250
korjeek <75172377+korjeek@users.noreply.github.com>
2025-03-25 15:10:05 +0500

    new json

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    "id": 1,
+    "name": "name"
+  });
 });
 
 module.exports = router;
