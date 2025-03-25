a6a8e62e957c0411becda36b749afb01ce1695ba
Maxim <78272312+Minetoff@users.noreply.github.com>
2025-03-25 17:22:47 +0500

    5 tasks

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var our_object = {
+  items: [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+    { id: 4, name: "Diana" }
+  ]
+};
+
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(our_object);
+});
+
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+
+  newUser.id = our_object.items.length > 0
+      ? our_object.items[our_object.items.length - 1].id + 1
+      : 1;
+
+  our_object.items.push(newUser);
 });
 
 module.exports = router;
