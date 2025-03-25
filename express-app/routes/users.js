a55c0b2094a2d43afbdf3210305f29d79aa15a2a
wswfws <wswfws@ya.ru>
2025-03-25 15:21:14 +0500

    done 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+class User {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    toObject() {
+        return {id: this.id, name: this.name};
+    };
+}
+
+const users = [];
+users.push(new User(1, 'John Doe'));
+users.push(new User(2, 'Jane Smith'));
+users.push(new User(3, 'Alice Johnson'));
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+    res.send({items: users.map(t => t.toObject())});
 });
 
 module.exports = router;
