f00ef13c480a0b9f7f685833591b2c05514a0e99
Maksim <maksim.zaitsev@urfu.me>
2025-03-25 18:03:55 +0500

    John losho4ek

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    'items': [
+      {
+        'id': 1,
+        'name': 'Maksim'
+      },
+      {
+        'id': 2,
+        'name': 'Alex'
+      },
+      {
+        'id': 3,
+        'name': 'John'
+      }
+    ]
+  });
 });
+
+router.post('/', function(req, res, next) {
+  req.bod
+});
+
 
 module.exports = router;
