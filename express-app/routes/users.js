2ab253dda2fd28036a9f0cf27588bc8556b7eb10
Julik <9045449@gmail.com>
2025-03-25 15:41:41 +0500

    5.1

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,33 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+let usersStorage = {
+  items: [
+    {
+      id: 1,
+      name: 'Julik'
+    },
+    {
+      id: 2,
+      name: 'renover'
+    },
+    {
+      id: 3,
+      name: 'meow'
+    },
+    {
+      id: 4,
+      name: 'Olega Rasin'
+    },
+    {
+      id: 5,
+      name: 'Dmitriy Kosolobik'
+    }
+  ],
+  nextId: 6
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const users = {
-    items: [
-      {
-        id: 1,
-        name: 'Julik'
-      },
-      {
-        id: 2,
-        name: 'renover'
-      },
-      {
-        id: 3,
-        name: 'meow'
-      },
-      {
-        id: 4,
-        name: 'Olega Rasin'
-      },
-      {
-        id: 5,
-        name: 'Dmitriy Kosolobik'
-      }
-    ]
+  res.json(usersStorage);
+});
+
+router.post('/', function(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+
+  const newUser = {
+    id: usersStorage.nextId++,
+    name: req.body.name
   };
-  res.json(users);
+
+  usersStorage.items.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
