048a158763fec6d9811d300fc8be3dad38f73861
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2025-03-25 15:12:27 +0500

    add 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+let users = {items : [{"id": 1, "name": "name"}, {"id": 2, "name": "Name"}]};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(users);
 });
 
 module.exports = router;
