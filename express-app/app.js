da0ef6363b2089e0eed4dea81e2644578dd8091c
S4ske <sany1111.ru@mail.ru>
2025-03-25 18:07:50 +0500

    1213

--- express-app/app.js
+++ express-app/app.js
@@ -13,7 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    if (req.query.auth !== 'true') {
+        return res.status(401).json({ "Unlucky": true })
+    }
 
+    next()
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
