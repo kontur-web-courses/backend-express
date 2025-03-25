af6657cdf547539fa0dee305ed2d996e0d8ac555
g_host <@>
2025-03-25 15:28:49 +0500

    получение, создание юзеров

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
-});
+let allUsers = [];
 
+/* GET users listing. */
+router.get('/',
+    function (req, res, next) {
+        res.send({
+            items: allUsers
+        });
+    });
+
+/* POST - create new user */
+router.post('/',
+    function (req, res, next) {
+    let newUser = req.body;
+    allUsers.push(newUser);
+        res.status(201).send(newUser);
+    });
 module.exports = router;
