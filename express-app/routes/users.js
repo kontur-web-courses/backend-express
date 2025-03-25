feeec9d0f5b7aa044ccf710bda698b82151c5e82
kusSsa <kuskovaevgenia3083@gmail.com>
2025-03-25 17:17:24 +0500

    первые задания

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+let users = {
+    items: [
+        { id: 1, name: "User 1" },
+        { id: 2, name: "User 2" },
+        { id: 3, name: "User 3" }
+    ],
+    nextId: 4
+};
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+    res.send(users);
+})
+
+router.post('/', function (req, res, next) {
+    const newUser = {
+        id: users.nextId++,
+        name: req.body.name,
+    };
+
+    users.items.push(newUser);
+    users.nextId++;
+
+    res.status(200).json(newUser);
 });
 
 module.exports = router;
