cda2de6b92251b53550562de784fe0314ff9b242
murad01 <adylovmurad@gmail.com>
2025-03-25 18:17:43 +0500

    finish

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text)`);
+
+
 let users = [
   { id: 1, name: "хопесима" },
   { id: 2, name: "райан гослинг" },
@@ -8,25 +15,22 @@ let users = [
 ];
 
 // GET /users
-router.get('/', (req, res) => {
-  res.json({ items: users });
+router.get('/', function(req, res, next) {
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+      if (err) {
+          console.log(err);
+      } else {
+          res.send(rows);
+      }
+  });
 });
 
 // POST /users
-router.post('/', (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    return res.status(400).json({ error: "Имя обязательно" });
-  }
-
-  const newUser = {
-    id: users.length + 1,
-    name
-  };
-
-  users.push(newUser);
-  res.status(201).json(newUser); // 201 Created
+router.post('/', function(req, res, next) {
+  let name = req.body.name;
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  db.run(insert, [name]);
+  res.send(req.body)
 });
 
 module.exports = router;
