5b3c92532e82d2368f1ba7c5281a93733efbc2fe
VladimirApter <vova.apter@gmail.com>
2025-03-25 15:18:43 +0500

    i just wanna roliroliroli

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  const users = [
+    { id: 1, name: 'Артем' },
+    { id: 2, name: 'Вова' },
+    { id: 3, name: 'Гога' }
+  ];
+
+  res.json({ items: users });
 });
 
 module.exports = router;
