4e47798504c54fd17d56029be9916a85f7cee462
Nataly505 <dmitrievan730@gmail.com>
2025-03-25 15:23:42 +0500

    task 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+
+  const object = {
+    items: [
+      {
+        "id": 1,
+        "name": "наточка"
+      }
+    ]
+  }
+  res.send(object);
 });
 
 module.exports = router;
