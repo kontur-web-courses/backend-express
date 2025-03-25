60ed8e42d08bd239da030a429a8665c5fe226821
Alexander Iarmoshenko <a2398253@gmail.com>
2025-03-25 17:08:25 +0500

    Task 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [
+      {
+        "id": 1,
+        "name": "name"
+      }
+    ]
+  });
 });
 
 module.exports = router;
