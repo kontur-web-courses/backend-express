5ce833a877fd1b562cbb0ed0391c755ae221a4ab
Tatiana <tanya-sok-05@mail.ru>
2025-03-25 17:34:24 +0500

    5 - post method

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,23 +1,23 @@
 var express = require("express");
 var router = express.Router();
 
+let users = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Donald",
+  },
+  {
+    id: 3,
+    name: "Trump",
+  },
+];
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  let users = [
-    {
-      id: 1,
-      name: "John",
-    },
-    {
-      id: 2,
-      name: "Donald",
-    },
-    {
-      id: 3,
-      name: "Trump",
-    },
-  ];
-
   // for (const id of Object.keys(req.query)) {
   //   users.push({
   //     id: id,
@@ -25,6 +25,30 @@ router.get("/", function (req, res, next) {
   //   });
   // }
   res.send({ users });
+});
+
+router.post("/", function(req, res) {
+  try {
+    const userData = req.body;
+
+    if (!userData.id || !userData.name) {
+      return res.status(400).json({ error: 'Missing required fields: id, name' });
+    }
+
+    if (users[userData.id]) {
+      return res.status(409).json({ error: 'User with this id already exists' });
+    }
+
+    users[userData.id] = {
+      id: userData.id,
+      name: userData.name
+    };
+
+    res.status(201).json(users[userData.id]);
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 module.exports = router;
