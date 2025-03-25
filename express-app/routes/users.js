313b92a4e1fc1aecf197b640713e26cbf126a934
futig <everymusic8@gmail.com>
2025-03-25 15:42:46 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+  ];
+  res.json({ items: users });
 });
 
 module.exports = router;
