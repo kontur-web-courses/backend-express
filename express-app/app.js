5fea56735869d3659f66b22bedb2e07669b66376
Jakob Ornysh <jornysh@gmail.com>
2025-03-25 17:13:56 +0500

    6

--- express-app/app.js
+++ express-app/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    if (req.query.auth === 'true') {
+        next();
+    } else {
+        res.status(401).send('Authentication required');
+    }
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
