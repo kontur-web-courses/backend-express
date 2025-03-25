f138590265b2da6427bc9026aa262eb776abe813
VladaKhazova <vladislava.khazova@urfu.me>
2025-03-25 15:58:02 +0500

    6

--- express-app/app.js
+++ express-app/app.js
@@ -13,6 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next)=>{
+    if (req.query.auth !== 'true'){
+        return req.status(401).json({
+            error: 'Not authorized',
+        });
+    }
+    next();
+})
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+let users = [
+  {"id": 1, "name": "Petya"},
+  {"id": 2, "name": "Anton"},
+  {"id": 3, "name": "Roma"}
+];
+
+
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json({items: users});
 });
+
+router.post('/', function(req, res, next) {
+  const {name} = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: '"name" обязательно' });
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
