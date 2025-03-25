d402dd33bb044dbb750b8374d46859875be3c1dc
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-25 15:25:10 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {"id": 1, "name": "Petya"},
+  {"id": 2, "name": "Anton"},
+  {"id": 3, "name": "Roma"}
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({
-    items: [
-      {"id": 1, "name": "Petya"},
-      {"id": 2, "name": "Anton"},
-      {"id": 3, "name": "Roma"}
-    ]
-  });
+  res.json({items: users});
 });
+
+router.post('/', function (req, res, next) {
+  const newUser = {
+    id: users.length ? users[users.length - 1].id + 1 : 1,
+    name: req.body.name,
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
