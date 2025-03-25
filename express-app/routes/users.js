8de2a9154bd26af6f908984e56a8db55c8449178
vladr <vladreva57@gmail.com>
2025-03-25 17:44:16 +0500

    number 6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,23 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    let result = {
-        "users": [{
-            "id": 1,
-            "name": "Biba"
-        }, {
-            "id": 2,
-            "name": "Boba"
-        }]
-    }
-    res.send(result);
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 router.post('/', function (req, res, next) {
     let user = req.body;
+    const name = user.name;
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [name]);
     res.send(user);
 })
 
-
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+/*
+db.run(`CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text)`);
+ */
 
 module.exports = router;
