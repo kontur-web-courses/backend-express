445654b3a181df80f1bade57fe9ce9efd927e626
Sawer <danilch4024@gmail.com>
2025-03-25 15:07:33 +0500

    items fix

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -4,9 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send({
+    "items": [{
     "id": 1,
     "name": "name"
-  });
+  }]});
 });
 
 module.exports = router;
