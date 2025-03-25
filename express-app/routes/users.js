af05176e36027a2a99da7f8fa6c574b2c6514274
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-03-25 15:14:39 +0500

    middleware commit

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  // res.send([{
+  //   "id": 1,
+  //   "name": "name"
+  // }, {
+  //   "id": 2,
+  //   "name": "admin"
+  // }, {
+  //   "id": 3,
+  //   "name": "sleepyCow"
+  // }]);
+
 });
 
+router.post('/users', function(req, res, next) {
+  let userData = req.body;
+});
 module.exports = router;
