4e61d057087767a886441247747fe7cecbcb3280
makst <maks.tses@gmail.com>
2025-03-25 15:56:14 +0500

    Final

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,16 +3,28 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 var router = express.Router();
 
+
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+
 let users = [];
 
 router.post('/', (req, res) => {
-  users.push(req.body);
+    const name = req.body.name;
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [name]);
   return res.status(200)});
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(users);
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 module.exports = router;
