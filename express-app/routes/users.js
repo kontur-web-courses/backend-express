a5dea6b7b7851aa76db2aba6bd8f219c1e339a85
usiqwerty <benlogin1@protonmail.com>
2025-03-25 15:10:49 +0500

    task 5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({
-    "items": [{
+let users = [{
     "id": 1,
     "name": "name"
-  }]});
+}];
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send({
+        "items": users
+    });
+});
+
+router.post('/', function (req, res, next) {
+    const newUser = req.body;
+    users.push(newUser);
+
 });
 
 module.exports = router;
