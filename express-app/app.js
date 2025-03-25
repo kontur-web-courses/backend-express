fbaaa85d806e8bfa2dd82464cf85b0daf171978b
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-25 15:56:28 +0500

    final

--- express-app/app.js
+++ express-app/app.js
@@ -10,9 +10,17 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  if (req.query.auth !== 'true') {
+    return res.status(401).json({
+      error: 'Not authorized',
+    });
+  }
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
