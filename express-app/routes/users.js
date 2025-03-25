160104ca5ca6462e0cd6ef61fed47c5b0b0138ac
MeTStIL <stme560@gmail.com>
2025-03-25 15:26:21 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,27 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const arr = {
+  items: [
+    {
+      "id": 1,
+      "name": "Stas"
+    },
+
+    {
+      "id": 2,
+      "name": "Ruslan"
+    },
+
+    {
+      "id": 3,
+      "name": "Russia"
+    }
+  ]
+}
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
-  return {
-    items: [
-      {
-        "id": 1,
-        "name": "Stas"
-      },
-
-      {
-        "id": 2,
-        "name": "Ruslan"
-      },
-
-      {
-        "id": 3,
-        "name": "Russia"
-      }
-    ]
-  }
+  res.send(arr);
 });
+
+
+router.post('/', function(req, res, next) {
+  const newPerson  = req.body;
+  arr.items.push(newPerson);
+  res.send(arr);
+})
 
 module.exports = router;
