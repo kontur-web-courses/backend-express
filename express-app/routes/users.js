f96974ba20610c10fddfcfb3064de803a7209393
murad01 <adylovmurad@gmail.com>
2025-03-25 17:18:27 +0500

    add list

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json({
+    items: [
+      {
+        id: 1,
+        name: "Иван Иванов"
+      },
+      {
+        id: 2,
+        name: "Петр Петров"
+      },
+      {
+        id: 3,
+        name: "Сергей Сергеев"
+      }
+    ]
+  });
 });
 
 module.exports = router;
