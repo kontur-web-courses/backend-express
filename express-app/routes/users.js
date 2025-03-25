474ad2ffe01666ab0b7d1e3761aed39daab850d3
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-25 15:06:02 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [
+      {"id": 1, "name": "Petya"},
+      {"id": 2, "name": "Anton"},
+      {"id": 3, "name": "Roma"}
+    ]
+  });
 });
 
 module.exports = router;
