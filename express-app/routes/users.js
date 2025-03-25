04053a4a289eb165203fd6934c63bd6f6e32c341
F1Soda <golik.tima@gmail.com>
2025-03-25 15:17:27 +0500

    first

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [
+      {
+        "id": 1,
+        "name": "TIM"
+      },
+      {
+        "id": 2,
+        "name": "GOGA"
+      },
+      {
+        "id": 3,
+        "name": "JURA"
+      },
+      {
+        "id": 4,
+        "name": "ARTEMCHIK"
+      },
+      {
+        "id": 5,
+        "name": "VOLODYA"
+      }
+    ]
+  });
 });
 
 module.exports = router;
