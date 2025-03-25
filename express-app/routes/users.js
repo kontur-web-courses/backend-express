b2eb1df2b48d5941feeaed7909378406b859fc33
Shutcing <maksim19111@gmail.com>
2025-03-25 17:17:33 +0500

    task 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get("/", function (req, res, next) {
+  let users = [
+    {
+      id: 1,
+      name: "John",
+    },
+    {
+      id: 2,
+      name: "Donald",
+    },
+    {
+      id: 3,
+      name: "Trump",
+    },
+  ];
+
+  // for (const id of Object.keys(req.query)) {
+  //   users.push({
+  //     id: id,
+  //     user: req.query[id],
+  //   });
+  // }
+  res.send({ users });
 });
 
 module.exports = router;
