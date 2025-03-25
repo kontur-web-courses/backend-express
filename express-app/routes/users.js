847916f6d720339bb39b799dc6ac1edf53f1007d
fomidon <gangstaerzh@gmail.com>
2025-03-25 17:30:59 +0500

    1-5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const users = [];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get("/", function (req, res, next) {
+  res.send(users);
+});
+
+/* POST users listing */
+router.post("/", function (req, res, next) {
+  let newUser = req.body;
+  newUser.id = users.length > 0 ? users.length + 1 : 1;
+  users.push(newUser);
+  res.status(200).send(newUser);
 });
 
 module.exports = router;
