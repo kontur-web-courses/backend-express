e757ce69aef42936c8a9a6da0f9b2b33be17f3bf
Jakob Ornysh <jornysh@gmail.com>
2025-03-25 17:26:39 +0500

    7

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,18 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text)`);
+
 /* GET users listing. */
 var users = [{
   "id": 1,
   "name": "name"
 }]
 
-router.get('/', function(req, res, next) {
-  res.send(users);
+router.get('/', function (req, res, next) {
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
-router.post('/', function(req, res, next) {
-  users.push(req.body);
+router.post('/', function (req, res, next) {
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  console.log(req.body.name);
+  db.run(insert, [req.body.name]);
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
 })
 
 module.exports = router;
