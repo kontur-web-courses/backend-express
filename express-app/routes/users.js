f127882317b67b70096739b5c2e60937c7226e63
Bu16a <147602417+Bu16a@users.noreply.github.com>
2025-03-25 17:21:48 +0500

    Update users.js

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+let usersStorage = {
+  items: [
+    {
+      "id": 1,
+      "name": "Дмитрий Косолобов"
+    },
+    {
+      "id": 2,
+      "name": "Иван Симонов"
+    },
+    {
+      "id": 3,
+      "name": "Сергей Сергеев"
+    }
+  ]
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json(usersStorage);
+});
+
+router.post('/', function(req, res, next) {
+
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+
+const newUser = {
+    id: usersStorage.items.length + 1, 
+    name: req.body.name
+  };
+
+  usersStorage.items.push(newUser);
+
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
