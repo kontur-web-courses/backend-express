a23c80284f81541ff32c9a1025a3416dbd265fc3
Grinik767 <grigorikiseleff@yandex.ru>
2025-03-25 17:09:43 +0500

    1 - 4

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+    let result = {
+        "users": [{
+            "id": 1,
+            "name": "Biba"
+        }, {
+            "id": 2,
+            "name": "Boba"
+        }]
+    }
+    res.send(result);
 });
 
 module.exports = router;
