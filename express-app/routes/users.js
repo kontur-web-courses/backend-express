53a34ddf077c355778fb9e3450790be7d39cd0e0
Julik <9045449@gmail.com>
2025-03-25 15:26:07 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  const users = {
+    items: [
+      {
+        id: 1,
+        name: 'Julik'
+      },
+      {
+        id: 2,
+        name: 'renover'
+      },
+      {
+        id: 3,
+        name: 'meow'
+      },
+      {
+        id: 4,
+        name: 'Olega Rasin'
+      },
+      {
+        id: 5,
+        name: 'Dmitriy Kosolobik'
+      }
+    ]
+  };
+  res.json(users);
 });
 
 module.exports = router;
