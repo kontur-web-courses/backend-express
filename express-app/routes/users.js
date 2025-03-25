d2b0bd16839ab3782fcb01e5f54208c4717c5ea6
Sawer <danilch4024@gmail.com>
2025-03-25 15:15:20 +0500

    7

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text)`);
+
+function insertUser(name) {
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  db.run(insert, [name]);
+}
+
+function getUsers(res) {
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({
-    "items": [{
-    "id": 1,
-    "name": "name"
-  }]});
+  getUsers(res);
 });
 
 module.exports = router;
