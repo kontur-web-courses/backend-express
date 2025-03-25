f81c676327a1f6f63cca858976cb6e9c6888165b
MeTStIL <stme560@gmail.com>
2025-03-25 15:07:00 +0500

    2

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -4,6 +4,24 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Hello, world!');
+  return {
+    items: [
+      {
+        "id": 1,
+        "name": "Stas"
+      },
+
+      {
+        "id": 2,
+        "name": "Ruslan"
+      },
+
+      {
+        "id": 3,
+        "name": "Russia"
+      }
+    ]
+  }
 });
 
 module.exports = router;
