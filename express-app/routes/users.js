79358f81a8bddcb1e837ed185c6d9735c8160324
Mot1x <motixflawless@gmail.com>
2025-03-25 17:45:55 +0500

    5-6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
+const users = {
+  items: [
+    {
+      "id": 1,
+      "name": "Your mother"
+    }
+  ]
+};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send({
-    "id": 1,
+    "id": 2,
     "name": "name"
   });
 });
+
+router.post('/', function(req, res, next) {
+  const newUser = {
+    id: usersStorage.items.length + 1,
+    name: req.body.name
+  };
+
+  usersStorage.items.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
 
 module.exports = router;
